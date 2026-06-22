290cb07e26e878c7423551e9d1357dde25211352
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2026-06-22 13:46:03 +0530

    Add maintenance page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
-import { Home } from './pages/Home';
-export function App() {
-  return <Home />;
+// import React from 'react';
+// import { Home } from './pages/Home';
+// export function App() {
+//   return <Home />;
+// }
+
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center",
+      }}
+    >
+      <h1>🚧 Under Maintenance</h1>
+      <p>We're updating our website. Please visit again soon.</p>
+    </div>
+  );
 }
+
+export default App;
